033d3d510905042c1ff109f4b6a90544391fddb1
黃筠蘋 <huangyunping@sd204092.ya.ab.nthu.edu.tw>
2024-04-08 20:58:38 +0800

    test tf 10 episode

--- 107062105_hw2_test.py
+++ 107062105_hw2_test.py
@@ -1,14 +1,111 @@
-import gym
 import random
+import gym
+import numpy as np
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras import backend as K
+from nes_py.wrappers import JoypadSpace
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
+import tensorflow as tf
 
-class Agent(object):
-    """Agent that acts randomly."""
+EPISODES = 5000
+
+
+class SkipFrame(gym.Wrapper):
+    def __init__(self, env, skip):
+        super().__init__(env)
+        self._skip = skip
+
+    def step(self, action): 
+        total_reward = 0.0
+        for i in range(self._skip):
+            # Accumulate reward and repeat the same action
+            state, reward, done, info = self.env.step(action)
+            total_reward += reward
+            if done:
+                break
+        return state, total_reward, done, info
+
+def preprocess_state(state):
+    #convert to grayscale
+    luminance = np.array([0.299, 0.587, 0.114])
+    grayscale_state = np.dot(state, luminance)
+    grayscale_state  = np.clip(grayscale_state , 0, 255).astype(np.uint8)
+    # print('grayscale state shape',grayscale_state.shape)
+    #resize to 120*128
+    grayscale_state = grayscale_state[::2, ::2]
+    # print('resize state shape',grayscale_state.shape)
+    return grayscale_state
+
+class DQNAgent:
     def __init__(self):
-        self.action_space = gym.spaces.Discrete(12)
-        pass
+        # self.state_size = state_size
+        # self.action_size = action_size
+        self.memory = deque(maxlen=1000000)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.99
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+        self.target_model = self._build_model()
+        self.update_target_model()
+        self.load("./mario-ddqn.h5")
 
-    def act(self, observation):
-        right_action = [0,1,2,3,4,5,10,11]
-        action = random.choice(right_action)
-        return action    
-        # return self.action_space.sample()
+
+    def _huber_loss(self, y_true, y_pred, clip_delta=1.0):
+        error = y_true - y_pred
+        cond  = K.abs(error) <= clip_delta
+
+        squared_loss = 0.5 * K.square(error)
+        quadratic_loss = 0.5 * K.square(clip_delta) + clip_delta * (K.abs(error) - clip_delta)
+
+        return K.mean(tf.where(cond, squared_loss, quadratic_loss))
+
+    def _build_model(self):
+        # Neural Net for Deep-Q learning Model
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss=self._huber_loss,
+                      optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def update_target_model(self):
+        # copy weights from model to target_model
+        self.target_model.set_weights(self.model.get_weights())
+
+    def memorize(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            action_list = list(range(12))  # List of integers from 0 to 11
+            weights = [0.05, 0.1, 0.1, 0.1, 0.1, 0.1, 0.05, 0.05, 0.05, 0.05, 0.1, 0.1]
+            return random.choices(action_list, weights=weights, k=1)[0]
+        act_values = self.model.predict(state,verbose = 0)
+        return np.argmax(act_values[0])  # returns action
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = self.model.predict(state,verbose = 0)
+            if done:
+                target[0][action] = reward
+            else:
+                t = self.target_model.predict(next_state,verbose = 0)[0]
+                target[0][action] = reward + self.gamma * np.amax(t)
+            self.model.fit(state, target, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
+
